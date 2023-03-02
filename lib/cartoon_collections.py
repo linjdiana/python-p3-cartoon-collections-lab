1229e848ed492c58834717bb1e9942280eb1ee83
+ <+>
2023-03-01 17:36:20 -0800

    done

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,23 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    for i in range(len(names)):
+        print(f"{i+1}. {names[i]}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    newlist = []
+    for call in planeteer_calls:
+        newlist.append(call[0].upper() + call[1:]
+                       + "!")
+    return newlist
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(callz):
+    for call in callz:
+        if len(call) > 4:
+            return True
+    else: 
+        return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(cheezes):
+    for i in range(len(cheezes)):
+        if cheezes[i]  == 'cheddar' or cheezes[i] == 'gouda' or cheezes[i] == 'camembert':
+            return cheezes[i]
+    return None
